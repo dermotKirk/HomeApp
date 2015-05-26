d253ed3ba2a4b428d6caea4e91ff7cb5b857f082
David Maxwell <Dave@Davids-MacBook-Pro.local>
2015-05-26 23:13:52 +0100

    Updated scripts

--- build.fsx
+++ build.fsx
@@ -1,13 +1,18 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
+#load "build-helpers.fsx"
 
 open Fake
+open BuildHelpers
 
 Target "common-build" (fun () ->
+    RestorePackages "TodoPCL.sln"
 
+    MSBuild "Todo/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ] [ "TodoPCL.sln" ] |> ignore
 )
 
 Target "common-tests" (fun () ->
-
+    RunNUnitTests "Todo/bin/Debug/TodoPCL.Tests.dll"
+    "Todo/bin/Debug/testresults.xml" |> ignore
 )
 
 "common-build"
